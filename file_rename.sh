286e95a3a96a5c8aa8cad7455eb58a362128dda2
Robert D. Chin <rdchin@yahoo.com>
2017-03-10 02:28:00 -0500

    Create README file
    This script renames files to make the file names compatible with most Operating Systems.

---
+++ file_rename.sh
@@ -0,0 +1,2 @@
+This script will replace or remove certain characters in a file name to make the file name compatible with most Operating systems.
+Colons, spaces, double-dashes, exclamation marks, question marks, quotation marks, etc.
