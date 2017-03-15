3d086f27e00be21645d7c8e8fe353b9e67ecab24
Robert D. Chin <rdchin@yahoo.com>
2017-03-15 12:36:44 -0400

    Added description and enhancement.
    Added a brief description.
    Added deletion of ampersands and percent signs from file names.

--- file_rename.sh
+++ file_rename.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 #
-VERSION="2017-03-10 01:47"
+VERSION="2017-03-15 12:20"
+#
+# Summary:
+#
+# If you save articles from web pages as PDF files and use the title 
+# of the article as the PDF file name, you will probably end up with
+# various punctuation marks in your file name which are incompatible
+# or undesirable for any given Operating System (i.e. Unix, Linux,
+# Microsoft, Apple, Android).
+#
+# Such file names derived from article titles often will contain
+# punctuation marks such as "?", "!", "/", "&", "%".
+#
+# This script was written to enforce some of the file naming conventions
+# to ensure inter-Operating System compatibility.
+#
+# It does not enforce all common file naming conventions, but only some
+# of the more common ones. See comments below for comprehensive list.
+#
+# You may easily add more naming conventions in the "Start Main Program"
+# section of the script using the existing code as a template.
+# 
+# 
 #
 #@ CODE HISTORY
 #@
@@ -226,68 +248,92 @@ while [  "$RECUR" != "1" -a "$RECUR" != "0" ]
 echo -n "Start time: " | tee $LOG_FILE
 date  | tee -a $LOG_FILE
 #
+# Compatible file names for most Operating Systems.
+#
+# Do not use the following characters:
+# # pound           < left angle bracket        $ dollar sign           + plus sign
+# % percent         > right angle bracket       ! exclamation point     ` backtick
+# & ampersand       * asterisk                  ' single quotes         | pipe
+# { left bracket    ? question mark             " double quotes         = equal sign
+# } right bracket   / forward slash             : colon
+# \ back slash      blank spaces                @ at sign
+#
+# File names must not begin or end with a <space>, <period>, <hyphen>, or <underline>
+# File names should be shorter than 31 characters for compatibility with older (1950-1990's) Operating Systems.
+# File names should use lower-case characters as most are case-sensitive.
+#
+# The order of operations below is important especially for the "find and replace" operations.
+#
 # 1. Replace <colon><space> with <two-dashes> in file name.
-f_banner "16 of 16 Replace <colon><space> with <two-dashes> in file name"
+f_banner "18 of 18 Replace <colon><space> with <two-dashes> in file name"
 f_find_and_replace ": " "--" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 2. Replace <colon> with <underscore> in file name.
-f_banner "15 of 16 Replace <colon> with <underscore> in file name"
+f_banner "17 of 18 Replace <colon> with <underscore> in file name"
 f_find_and_replace ":" "_" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 3. Replace <space> with <underscore> in file name.
-f_banner "14 of 16 Replace <space> with <underscore> in file name"
+f_banner "16 of 18 Replace <space> with <underscore> in file name"
 f_find_and_replace " " "_" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 4. Replace <double-dash> with <two-dashes> in file name.
-f_banner "13 of 16 Replace <double-dash> with <two-dashes> in file name"
+f_banner "15 of 18 Replace <double-dash> with <two-dashes> in file name"
 f_find_and_replace "—" "--" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 5. Remove <exclamation-mark> in file name.
-f_banner "12 of 16 Remove <exclamation-mark> in file name"
+f_banner "14 of 18 Remove <exclamation-mark> in file name"
 f_find_and_delete "!" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 6. Remove <question-mark> in file name.
-f_banner "11 of 16 Remove <question-mark> in file name"
+f_banner "13 of 18 Remove <question-mark> in file name"
 f_find_and_delete "\?" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
+# 6. Remove <percent-sign> in file name.
+f_banner "12 of 18 Remove <percent-sign> in file name"
+f_find_and_delete "%" $TARGET_DIR $FOPTIONS $ROPTIONS
+#
+# 6. Remove <ampersand> in file name.
+f_banner "11 of 18 Remove <ampersand> in file name"
+f_find_and_delete "&" $TARGET_DIR $FOPTIONS $ROPTIONS
+#
 # 7. Remove <single-quote> in file name.
-f_banner "10 of 16 Remove <single-quote> in file name"
+f_banner "10 of 18 Remove <single-quote> in file name"
 f_find_and_delete "'" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 8. Remove <single-right-quote> in file name.
-f_banner "9 of 16 Remove <single-right-quote> in file name"
+f_banner "09 of 18 Remove <single-right-quote> in file name"
 f_find_and_delete "’" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 9. Remove <double-quote> in file name.
-f_banner "8 of 16 Remove <double-quote> in file name"
+f_banner "08 of 18 Remove <double-quote> in file name"
 f_find_and_delete "\"" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 10. Remove <right-double-quote> in file name.
-f_banner "7 of 16 Remove <right-double-quote> in file name"
+f_banner "07 of 18 Remove <right-double-quote> in file name"
 f_find_and_delete "”" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 11. Remove <left-double-quote> in file name.
-f_banner "6 of 16 Remove <left-double-quote> in file name"
+f_banner "06 of 18 Remove <left-double-quote> in file name"
 f_find_and_delete "“" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 12. Replace <underscore-period>  with <period> in file name.
-f_banner "5 of 16 Replace <underscore-period>  with <period> in file name"
+f_banner "05 of 18 Replace <underscore-period>  with <period> in file name"
 f_find_and_replace "_\." "." $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 13. Replace <triple-underscore> with <underscore> in file name.
-f_banner "4 of 16 Replace <triple-underscore> with <underscore> in file name"
+f_banner "04 of 18 Replace <triple-underscore> with <underscore> in file name"
 f_find_and_replace "___" "_" $TARGET_DIR $FOPTIONS $ROPTIONS
 # 
 # 14. Replace <double-underscore> with <underscore> in file name.
-f_banner "3 of 16 Replace <double-underscore> with <underscore> in file name"
+f_banner "03 of 18 Replace <double-underscore> with <underscore> in file name"
 f_find_and_replace "__" "_" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 15. Replace <two-dashes><underscore> with <two-dashes> in file name.
-f_banner "2 of 16 Replace <two-dashes><underscore> with <two-dashes> in file name."
+f_banner "02 of 18 Replace <two-dashes><underscore> with <two-dashes> in file name."
 f_find_and_replace "--_" "--" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 # 16. Replace <underscore><two-dashes> with <two-dashes> in file name.
-f_banner "1 of 16 Replace <underscore><two-dashes> with <two-dashes> in file name."
+f_banner "01 of 18 Replace <underscore><two-dashes> with <two-dashes> in file name."
 f_find_and_replace "_--" "--" $TARGET_DIR $FOPTIONS $ROPTIONS
 #
 echo -n "Finish time: " | tee -a $LOG_FILE
